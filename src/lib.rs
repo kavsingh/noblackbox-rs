6c583ef26b60e8da7a72b51c187f85f177ddb5a5
Kav Singh <flakisms@gmail.com>
2024-05-11 21:17:33 +0200

    console log, interactivity

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,15 @@ pub mod fileserv;
 pub mod layouts;
 pub mod pages;
 
+use log::Level;
+
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
-	use crate::app::*;
+	#[allow(unused_imports)]
+	use app::*;
+
+	console_log::init_with_level(Level::Trace).expect("error initializing log");
 	console_error_panic_hook::set_once();
-	leptos::mount_to_body(App);
+	leptos::leptos_dom::HydrationCtx::stop_hydrating();
 }
--- src/pages/train.rs
+++ src/pages/train.rs
@@ -1,6 +1,8 @@
+use crate::components::{
+	button::Button,
+	card::{Card, CardContent, CardHeader, CardTitle},
+};
 use leptos::*;
-
-use crate::components::card::{Card, CardContent, CardHeader, CardTitle};
 
 #[component]
 pub fn Train() -> impl IntoView {
@@ -12,8 +14,47 @@ pub fn Train() -> impl IntoView {
 				</CardTitle>
 			</CardHeader>
 			<CardContent>
-				<canvas id="train" width="400" height="400"></canvas>
+				<Sketchpad/>
 			</CardContent>
 		</Card>
+	}
+}
+
+type Path = Vec<(i32, i32)>;
+
+#[island]
+pub fn Sketchpad() -> impl IntoView {
+	let (paths, set_paths) = create_signal(Vec::<Path>::new());
+
+	let on_pointer_down = move |ev: ev::PointerEvent| {
+		let x = ev.client_x();
+		let y = ev.client_y();
+
+		set_paths.update(|paths| {
+			paths.push(vec![(x, y)]);
+		});
+	};
+
+	let on_undo_click = move |_| {
+		set_paths.update(|paths| {
+			paths.pop();
+		});
+	};
+
+	create_effect(move |_| {
+		log::info!("paths: {:?}", paths.get());
+	});
+
+	view! {
+		<div class="flex gap-4">
+			<canvas
+				on:pointerdown=on_pointer_down
+				id="train"
+				width="400"
+				height="400"
+				class="bg-white stroke-black"
+			></canvas>
+			<Button on:click=on_undo_click>Undo</Button>
+		</div>
 	}
 }
