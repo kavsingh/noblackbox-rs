10fa6728b0ef53623837a1390c1a77dda172458b
Kav Singh <flakisms@gmail.com>
2024-05-13 03:35:57 +0200

    simplify

--- src/pages/train.rs
+++ src/pages/train.rs
@@ -8,9 +8,12 @@ use crate::components::{
 use leptos::*;
 
 type Paths = Vec<Vec<(i32, i32)>>;
-type Drawing = (&'static str, Option<Paths>);
 
-#[derive(serde::Serialize, Debug)]
+const TRAINING_LABELS: [&str; 8] = [
+	"car", "fish", "house", "tree", "bicycle", "guitar", "pencil", "clock",
+];
+
+#[derive(serde::Serialize, Debug, Clone)]
 struct TrainingData {
 	session: i32,
 	student: &'static str,
@@ -37,48 +40,29 @@ pub fn Train() -> impl IntoView {
 
 #[island]
 fn Trainer() -> impl IntoView {
-	let (drawings, set_drawings) = create_signal::<Vec<Drawing>>(vec![
-		("car", None),
-		("fish", None),
-		("house", None),
-		("tree", None),
-		("bicycle", None),
-		("guitar", None),
-		("pencil", None),
-		("clock", None),
-	]);
+	let (session, set_session) = create_signal(TrainingData {
+		session: 123456,
+		student: "__leptos__",
+		drawings: HashMap::<&str, Paths>::new(),
+	});
 
-	let current_label = move || drawings().iter().find(|d| d.1.is_none()).map(|d| d.0);
+	let current_label = move || {
+		TRAINING_LABELS
+			.iter()
+			.find(|l| !session().drawings.contains_key(*l))
+			.copied()
+	};
 
 	let save_drawing = move |paths: Paths| {
-		let label = match current_label() {
-			Some(l) => l,
-			None => return,
+		if let Some(label) = current_label() {
+			set_session.update(|current| {
+				current.drawings.insert(label, paths);
+			});
 		};
-
-		set_drawings.update(|current| {
-			if let Some(index) = current.iter().position(|d| d.0 == label) {
-				current[index].1 = Some(paths);
-			}
-		})
 	};
 
 	let save_session = move |_| {
-		let mut drawing_map = HashMap::<&str, Paths>::new();
-
-		drawings.get_untracked().iter().for_each(|drawing| {
-			if let Some(paths) = &drawing.1 {
-				drawing_map.insert(drawing.0, paths.clone());
-			}
-		});
-
-		let data = TrainingData {
-			session: 123456,
-			student: "__leptos__",
-			drawings: drawing_map,
-		};
-
-		log::debug!("save session {:?}", data)
+		log::debug!("save session {:?}", session());
 	};
 
 	view! {
