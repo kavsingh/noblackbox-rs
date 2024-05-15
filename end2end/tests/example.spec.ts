47597958df1581f8e52ae4565022a22b6d318b1a
Kav Singh <flakisms@gmail.com>
2024-05-15 20:21:10 +0200

    deps up

--- end2end/tests/example.spec.ts
+++ end2end/tests/example.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from "@playwright/test";
 
 test("homepage has title and heading text", async ({ page }) => {
 	await page.goto("http://localhost:3000/");
-	await expect(page).toHaveTitle("Welcome to Leptos");
-	await expect(page.locator("h1")).toHaveText("Welcome to Leptos!");
+	await expect(page).toHaveTitle("noblackbox");
+	await expect(page.locator("h1")).toHaveText("noblackbox");
 });
