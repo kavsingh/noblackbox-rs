30ae178f2be95c6910cb8794793c026ae2204cdd
Kav Singh <flakisms@gmail.com>
2025-02-28 20:52:13 +0100

    refactor: use biome

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -44,9 +44,9 @@ export default {
 				},
 			},
 			borderRadius: {
-				xl: `calc(var(--radius) + 4px)`,
-				lg: `var(--radius)`,
-				md: `calc(var(--radius) - 2px)`,
+				xl: "calc(var(--radius) + 4px)",
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
 				sm: "calc(var(--radius) - 4px)",
 			},
 			fontFamily: {
@@ -54,5 +54,4 @@ export default {
 			},
 		},
 	},
-	plugins: [],
 } satisfies Config;
