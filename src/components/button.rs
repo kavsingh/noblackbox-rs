2b8d1043639a1c913a38b6041be7e5d042dd587c
Kav Singh <flakisms@gmail.com>
2025-02-28 22:01:46 +0100

    refactor: tailwind 4

--- src/components/button.rs
+++ src/components/button.rs
@@ -1,12 +1,13 @@
-// https://ui.shadcn.com/docs/components/button
+// https://github.com/shadcn-ui/ui/blob/main/apps/v4/registry/new-york-v4/ui/button.tsx
+// TODO: variants, somehow. maybe https://github.com/gaucho-labs/tailwind-fuse?
 
 use leptos::*;
 
 #[component]
 pub fn Button(children: Children) -> impl IntoView {
 	view! {
-		<button class="inline-flex items-center justify-center h-9 px-4 py-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground">
-			{children()}
-		</button>
+		<button class="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive \
+		border border-input bg-background shadow-xs hover:bg-accent hover:text-accent-foreground \
+		h-9 px-4 py-2 has-[>svg]:px-3">{children()}</button>
 	}
 }
--- src/components/card.rs
+++ src/components/card.rs
@@ -1,11 +1,11 @@
-// https://ui.shadcn.com/docs/components/card
+// https://github.com/shadcn-ui/ui/blob/main/apps/v4/registry/new-york-v4/ui/card.tsx
 
 use leptos::*;
 
 #[component]
 pub fn Card(children: Children) -> impl IntoView {
 	view! {
-		<div class="rounded-xl border border-border bg-card text-card-foreground shadow">
+		<div class="bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm">
 			{children()}
 		</div>
 	}
@@ -13,25 +13,25 @@ pub fn Card(children: Children) -> impl IntoView {
 
 #[component]
 pub fn CardHeader(children: Children) -> impl IntoView {
-	view! { <div class="flex flex-col space-y-1.5 p-6">{children()}</div> }
+	view! { <div class="flex flex-col gap-1.5 px-6">{children()}</div> }
 }
 
 #[component]
 pub fn CardTitle(children: Children) -> impl IntoView {
-	view! { <div class="font-semibold leading-none tracking-tight">{children()}</div> }
+	view! { <div class="leading-none font-semibold">{children()}</div> }
 }
 
 #[component]
 pub fn CardDescription(children: Children) -> impl IntoView {
-	view! { <div class="text-sm text-muted-foreground">{children()}</div> }
+	view! { <div class="text-muted-foreground text-sm">{children()}</div> }
 }
 
 #[component]
 pub fn CardContent(children: Children) -> impl IntoView {
-	view! { <div class="p-6 pt-0">{children()}</div> }
+	view! { <div class="px-6">{children()}</div> }
 }
 
 #[component]
 pub fn CardFooter(children: Children) -> impl IntoView {
-	view! { <div class="flex items-center p-6 pt-0">{children()}</div> }
+	view! { <div class="flex items-center px-6">{children()}</div> }
 }
